a7021ba536b8c298848967a369dcb08bb4bcaf3c
evan <168372878+jamm0316@users.noreply.github.com>
2024-08-10 06:30:20 +0900

    Update 1316.py

--- 구현/실버/실버5_1316_그룹 단어 체커/1316.py
+++ 구현/실버/실버5_1316_그룹 단어 체커/1316.py
@@ -1,44 +1,38 @@
 import sys
 
 def main():
-    """입력을 읽고 그룹 단어를 확인하는 메인 함수."""
-    input_data = sys.stdin.read()
-    data = input_data.split()
-
-    # 입력 데이터 파싱
-    query_count = int(data[0])  # 쿼리 갯수
-    queries = data[1:]  # 쿼리 목록
-
-    # 쿼리로 초기화된 체커 생성
-    checker = GroupWordChecker(queries)
+    #입력값 파싱
+    input = sys.stdin.read()
+    data = input.split()
     
-    # 결과 출력
-    print(checker.count_group_words())
-    
+    N = int(data[0])  # 쿼리 갯수
+    queries = data[1:]  # 쿼리들
+
+    # 그룹 단어 갯수 세기
+    count_group_word = GroupWordChecker().count_word(queries)
+    print(count_group_word)
+
 class GroupWordChecker:
-    def __init__(self, queries):
-        self.queries = queries
-
-    def count_group_words(self):
-        """제공된 쿼리에서 그룹 단어의 수를 계산합니다."""
-        group_word_count = 0
-        for query in self.queries:
-            if self.is_group_word(query):
-                group_word_count += 1
-        return group_word_count
-
-    def is_group_word(self, query):
-        """주어진 쿼리가 그룹 단어인지 확인합니다."""
-        seen_chars = set()
-        previous_char = None
-
-        for char in query:
-            if char != previous_char:
-                if char in seen_chars:
+    # mothod. 그룹 단어인지 체크 // return boolean
+    def check_word(self, query):
+        seen = set()
+        previous_word = None
+        for word in query:
+            if word != previous_word:
+                if word in seen:
                     return False
-                seen_chars.add(char)
-                previous_char = char
+            seen.add(word)
+            previous_word = word
         return True
-    
+
+    # mothod. 그룹 갯수 체크 // return int
+    def count_word(self, queries):
+        total_count = 0
+        for query in queries:
+            check_group_word = GroupWordChecker().check_word(query)
+            if check_group_word:
+               total_count += 1
+        return total_count
+
 if __name__ == '__main__':
     main()
