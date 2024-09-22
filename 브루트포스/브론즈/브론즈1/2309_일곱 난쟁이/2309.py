c71958a5b44b750a235b81cde02835af58ac271c
evan <168372878+jamm0316@users.noreply.github.com>
2024-09-23 01:37:44 +0900

    Update 2309.py

--- 브루트포스/브론즈/브론즈1/2309_일곱 난쟁이/2309.py
+++ 브루트포스/브론즈/브론즈1/2309_일곱 난쟁이/2309.py
@@ -1,24 +1,18 @@
 import sys
 
-input_data = sys.stdin.read()
-dwarfs_height = list(map(int, input_data.splitlines()))
-
 def find_origin_dwarfs(dwarfs_height):
-    SUM_DWARFS_HEIGHT = 100
-    total_sum = sum(dwarfs_height)
+    all_dwarfs_height = 100
     number_of_dwarfs = len(dwarfs_height)
-
-    found = False
+    total_sum = sum(dwarfs_height)
     for i in range(number_of_dwarfs):
         for j in range(i + 1, number_of_dwarfs):
-            if total_sum - dwarfs_height[i] - dwarfs_height[j] == SUM_DWARFS_HEIGHT:
-                dwarfs_height = [dwarfs_height[x] for x in range(number_of_dwarfs) if x is not i and x is not j]
-                dwarfs_height.sort()
-                for height in dwarfs_height:
-                    print(height)
-                found = True
-                break
-        if found:
-            break
+            if  total_sum - dwarfs_height[i] - dwarfs_height[j] == all_dwarfs_height:
+                origin_dwarfs = [dwarfs_height[x] for x in range(number_of_dwarfs) if x != i and x != j]
+                return sorted(origin_dwarfs)
 
-find_origin_dwarfs(dwarfs_height)
+#입력값 파싱
+input_data = sys.stdin.read()
+dwarfs_height = [line for line in map(int, input_data.splitlines())]
+origin_dwarf = find_origin_dwarfs(dwarfs_height)
+for dwarf in origin_dwarf:
+    print(dwarf)
