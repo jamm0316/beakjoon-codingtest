4f5a188e44ca9b4f2c358bcf29afd07d3a87c29e
evan <168372878+jamm0316@users.noreply.github.com>
2024-09-27 23:09:58 +0900

    Update 1436.py

--- 브루트포스/실버/실버5/1436_종말의_수/1436.py
+++ 브루트포스/실버/실버5/1436_종말의_수/1436.py
@@ -11,3 +11,20 @@ def find_end_of_world_number(N):
 
 N = int(input())
 print(find_end_of_world_number(N))
+
+
+---
+
+#case 2
+def find_end_of_world_number(N):
+    number, count = 666, 0
+    
+    while count < N:
+        if '666' in str(number):
+            count += 1
+        number += 1
+    
+    return number - 1
+
+N = int(input("찾고 싶은 종말의 숫자를 입력하세요: "))
+print(find_end_of_world_number(N))
