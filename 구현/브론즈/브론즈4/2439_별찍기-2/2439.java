711fd153096647aca4c12621f5620a87fc37c831
evan <168372878+jamm0316@users.noreply.github.com>
2024-11-06 17:43:16 +0900

    Create 2439.java

---
+++ 구현/브론즈/브론즈4/2439_별찍기-2/2439.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = n - i; 0 < j; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
