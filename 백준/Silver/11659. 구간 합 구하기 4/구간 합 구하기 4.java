9f513279deea0256caf2b90dc84b8eeabffad43c
evan <168372878+jamm0316@users.noreply.github.com>
2024-12-03 04:43:10 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 500 ms, Memory: 53524 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,54 +1,31 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int sequenceNum;
-    static int session;
-    static int[] sequence;
-    static int[][] queries;
-
     public static void main(String[] args) throws IOException {
-        initData();
-        int[] prefixSum = makePrefixSum(sequenceNum, sequence);
-        calculatePrefixSum(prefixSum, queries);
-    }
-
-    private static void calculatePrefixSum(int[] prefixSum, int[][] queries) {
-        for (int[] query : queries) {
-            int start = query[0];
-            int end = query[1];
-
-            int eachSum = prefixSum[end] - prefixSum[start - 1];
-            System.out.println(eachSum);
-        }
-    }
-
-    private static int[] makePrefixSum(int sequenceNum, int[] sequence) {
-        int[] prefixSum = new int[sequenceNum];
-        for (int i = 1; i < sequenceNum; i++) {
-            prefixSum[i] = prefixSum[i - 1] + sequence[i];
-        }
-        return prefixSum;
-    }
-
-    private static void initData() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        sequenceNum = Integer.parseInt(st.nextToken()) + 1;
-        session = Integer.parseInt(st.nextToken());
-        sequence = new int[sequenceNum];
-        queries = new int[session][2];
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] sequence = new int[N + 1]; // 입력 배열 (1-based)
+        int[] prefixSum = new int[N + 1]; // Prefix Sum 배열 (1-based)
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i < sequenceNum; i++) {
+        for (int i = 1; i <= N; i++) {
             sequence[i] = Integer.parseInt(st.nextToken());
+            prefixSum[i] = prefixSum[i - 1] + sequence[i];
         }
 
-        for (int i = 0; i < session; i++) {
+        StringBuilder result = new StringBuilder(); // 출력 최적화
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            queries[i][0] = Integer.parseInt(st.nextToken());
-            queries[i][1] = Integer.parseInt(st.nextToken());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            result.append(prefixSum[end] - prefixSum[start - 1]).append("\n");
         }
+
+        System.out.print(result); // 결과 출력
     }
 }
