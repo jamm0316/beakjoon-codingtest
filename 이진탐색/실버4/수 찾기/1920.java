a80ca7e03c29a3391c1e37cd5d351e069708b8a4
evan <168372878+jamm0316@users.noreply.github.com>
2024-12-10 05:37:32 +0900

    Create 1920.java

---
+++ 이진탐색/실버4/수 찾기/1920.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+        int[] searchTargetArray = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            searchTargetArray[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(searchTargetArray);
+
+        st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int query = Integer.parseInt(st.nextToken());
+            sb.append(binarySearch(searchTargetArray, query)).append("\n");
+        }
+
+        bw.write(String.valueOf(sb));
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+
+    private static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                return 1;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return 0;
+    }
+}
