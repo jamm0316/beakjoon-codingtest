954be89e3fe1d744bbbf7d19582491b8ac56dbea
evan <168372878+jamm0316@users.noreply.github.com>
2024-12-11 08:19:00 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 100 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -7,34 +7,35 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
-        int queries = 1 << N;
-        List<int[]> taste = new ArrayList<>();
+        int[][] ingredient = new int[N][2];
 
         for (int i = 0; i < N; i++) {
-            String[] query = br.readLine().split(" ");
-
-            taste.add(Arrays.stream(query)
-                    .mapToInt(Integer::valueOf)
-                    .toArray());
+            st = new StringTokenizer(br.readLine());
+            ingredient[i][0] = Integer.parseInt(st.nextToken());
+            ingredient[i][1] = Integer.parseInt(st.nextToken());
         }
 
-
-        int minValue = Integer.MAX_VALUE;
-
-        for (int i = 1; i < queries; i++) {
-            int S = 1;
-            int B = 0;
-
-            for (int j = 0; j < N; j++) {
-                if ((i & (1 << j)) != 0) {
-
-                    S *= taste.get(j)[0];
-                    B += taste.get(j)[1];
-                }
-            }
-            minValue = Math.min(minValue, Math.abs(S - B));
-        }
+        int minValue = findMinTaste(N, ingredient);
 
         System.out.println(minValue);
+    }
+
+    private static int findMinTaste(int N, int[][] ingredient) {
+        int query = 1 << N;
+        int minValue = Integer.MAX_VALUE;
+
+        for (int i = 1; i < query; i++) {
+            int sour = 1;
+            int bitter = 0;
+
+            for (int j = 0; j < N; j++) {
+                if ((i & 1 << j) != 0) {
+                    sour *= ingredient[j][0];
+                    bitter += ingredient[j][1];
+                }
+            }
+            minValue = Math.min(minValue, Math.abs(sour - bitter));
+        }
+        return minValue;
     }
 }
