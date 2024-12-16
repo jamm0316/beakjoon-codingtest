bff73b847fb20a4e8c20e25504acb650368e1cea
evan <168372878+jamm0316@users.noreply.github.com>
2024-12-17 08:44:23 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 532 ms, Memory: 70180 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -2,21 +2,22 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuffer sb = new StringBuffer();
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         List<Integer>[] graph = new ArrayList[N + 1];
-        int[] parents = new int[N + 1];
+        Queue<Integer> queue = new LinkedList<>();
         boolean[] visited = new boolean[N + 1];
+        int[] parents = new int[N + 1];
 
+        //initialize graph
         for (int i = 1; i <= N; i++) {
             graph[i] = new ArrayList<>();
         }
-
         for (int i = 0; i < N - 1; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
@@ -25,18 +26,17 @@ public class Main {
             graph[b].add(a);
         }
 
-        Queue<Integer> queue = new LinkedList<>();
+        //search graph
         queue.add(1);
         visited[1] = true;
-
         while (!queue.isEmpty()) {
             int current = queue.poll();
 
             for (int neighbor : graph[current]) {
                 if (!visited[neighbor]) {
-                    visited[neighbor] = true;
-                    parents[neighbor] = current;
                     queue.add(neighbor);
+                    visited[current] = true;
+                    parents[neighbor] = current;
                 }
             }
         }
