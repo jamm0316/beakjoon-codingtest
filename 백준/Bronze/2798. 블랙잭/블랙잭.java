c4a94ff20c1263db39f3f59672f6d188f90942f0
evan <168372878+jamm0316@users.noreply.github.com>
2024-12-31 08:46:20 +0900

    [Bronze II] Title: 블랙잭, Time: 108 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,5 +1,5 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -7,22 +7,29 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
-        int target = Integer.parseInt(st.nextToken());
-        int[] card = Arrays.stream(br.readLine().split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
-        int result = Integer.MIN_VALUE;
+        int M = Integer.parseInt(st.nextToken());
+        int[] cards = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            cards[i] = Integer.parseInt(st.nextToken());
+        }
 
-        for (int i = 0; i < card.length; i++) {
-            for (int j = i+1; j < card.length; j++) {
-                for (int k = j+1; k < card.length; k++) {
-                    if (card[i] + card[j] + card[k] <= target) {
-                        result = Math.max(card[i] + card[j] + card[k], result);
+        int result = findMaxNumberOf3Cards(N, M, cards);
+        System.out.println(result);
+    }
+
+    private static int findMaxNumberOf3Cards(int N, int M, int[] cards) {
+        int result = Integer.MIN_VALUE;
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                for (int k = j + 1; k < N; k++) {
+                    int eachScore = cards[i] + cards[j] + cards[k];
+                    if (eachScore <= M) {
+                        result = Math.max(result, eachScore);
                     }
                 }
             }
         }
-
-        System.out.println(result);
+        return result;
     }
 }
