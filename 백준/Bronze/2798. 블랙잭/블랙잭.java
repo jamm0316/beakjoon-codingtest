f4261acf9d883c037d60a87143a08ce233b33542
evan <168372878+jamm0316@users.noreply.github.com>
2024-12-31 09:01:28 +0900

    [Bronze II] Title: 블랙잭, Time: 76 ms, Memory: 11668 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,38 +1,65 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-            static int M;
-            static int[] cards;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        cards = new int[N];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            cards[i] = Integer.parseInt(st.nextToken());
-        }
+	// N - 선택지 카드수, M - 한계
+	static int N, M;
 
-        int result = findMaxNumberOf3Cards(N, M, cards);
-        System.out.println(result);
-    }
+	static int[] card;
 
-    private static int findMaxNumberOf3Cards(int N, int M, int[] cards) {
-        int result = Integer.MIN_VALUE;
-        for (int i = 0; i < N; i++) {
-            for (int j = i + 1; j < N; j++) {
-                for (int k = j + 1; k < N; k++) {
-                    int eachScore = cards[i] + cards[j] + cards[k];
-                    if (eachScore <= M) {
-                        result = Math.max(result, eachScore);
-                    }
-                }
-            }
-        }
-        return result;
-    }
+	static int[] arr = new int[3];
+
+	static int result = 0;
+
+	static void combi(int cnt, int start) {
+
+		if (cnt == 3) {
+			int tmp = 0;
+			for (int i = 0; i < 3; i++) {
+				tmp += arr[i];
+			}
+			if (tmp <= M) {
+				result = Math.max(result, tmp);
+			}
+			
+			return;
+		}
+		
+		for (int i = start; i < N; i++) {
+			
+			arr[cnt] = card[i];
+			combi(cnt+1, i+1);
+			
+		}
+
+	}
+
+	// 조합 문제
+	// 중복 X
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		// 입력 받기
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		card = new int[N];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			card[i] = Integer.parseInt(st.nextToken());
+		}
+
+		// 함수 호출
+		combi(0, 0);
+		
+		System.out.println(result);
+
+	}
+
 }
