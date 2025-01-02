6912c0db0d37cb1a2f50814862dcc0611013f103
evan <168372878+jamm0316@users.noreply.github.com>
2025-01-03 06:10:27 +0900

    Create P11053.java

---
+++ DP/실버2/11053_가장 긴 증가하는 부분 수열/P11053.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int[] sequence = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int[] dp = new int[N];
+        Arrays.fill(dp, 1);
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (sequence[j] < sequence[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        int lis = Arrays.stream(dp).max().getAsInt();
+        System.out.println(lis);
+    }
+}
