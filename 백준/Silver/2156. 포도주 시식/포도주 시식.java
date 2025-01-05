8ed2f71245bbadc34a419004c20a7ccfb04ed7bd
evan <168372878+jamm0316@users.noreply.github.com>
2025-01-06 04:10:40 +0900

    [Silver I] Title: 포도주 시식, Time: 136 ms, Memory: 15496 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -1,41 +1,33 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        // 입력 받기
-        int n = scanner.nextInt();
-        int[] wine = new int[n];
-        for (int i = 0; i < n; i++) {
-            wine[i] = scanner.nextInt();
-        }
+        int N = Integer.parseInt(st.nextToken());
+        int[] wine = new int[N];
+        int[] dp = new int[N];
 
-        // DP 배열 초기화
-        int[] dp = new int[n];
-        
-        if (n == 1) {
-            System.out.println(wine[0]);
-            return;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            wine[i] = Integer.parseInt(st.nextToken());
         }
 
         dp[0] = wine[0];
-        if (n > 1) {
+        if (N > 1) {
             dp[1] = wine[0] + wine[1];
         }
-        if (n > 2) {
-            dp[2] = Math.max(dp[1], Math.max(wine[0] + wine[2], wine[1] + wine[2]));
+
+        if (N > 2) {
+            dp[2] = Math.max(dp[1], Math.max(wine[2] + dp[0], wine[1] + wine[2]));
         }
 
-        // 점화식을 사용하여 DP 계산
-        for (int i = 3; i < n; i++) {
-            dp[i] = Math.max(dp[i - 1],
-                     Math.max(dp[i - 2] + wine[i], dp[i - 3] + wine[i - 1] + wine[i]));
+        for (int i = 3; i < N; i++) {
+            dp[i] = Math.max(dp[i - 1], Math.max(wine[i] + dp[i - 2], wine[i] + wine[i - 1] + dp[i - 3]));
         }
 
-        // 최댓값 출력
-        System.out.println(dp[n - 1]);
-
-        scanner.close();
+        System.out.println(dp[N - 1]);
     }
 }
