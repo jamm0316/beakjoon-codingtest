7b05411ad1153299d138f5560abdb791ab375cc2
evan <168372878+jamm0316@users.noreply.github.com>
2025-01-07 08:40:59 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 108 ms, Memory: 13996 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int[][] queries = new int[N][2];
+        int[][] dp = new int[15][15];
+        int person = 0;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            queries[i][0] = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+            queries[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        dp[0][0] = 0;
+        dp[0][1] = 1;
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 1; j < dp[i].length; j++) {
+                if (i == 0) {
+                    dp[i][j] = j;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+
+        for (int i = 0; i < queries.length; i++) {
+            int a = queries[i][0];
+            int b = queries[i][1];
+            System.out.println(dp[a][b]);
+        }
+    }
+}
