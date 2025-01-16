fdb611543b160ff31109a712c69bb90ee64e2c6e
evan <168372878+jamm0316@users.noreply.github.com>
2025-01-17 04:57:34 +0900

    [Silver V] Title: 다리 놓기, Time: 116 ms, Memory: 15760 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,30 +1,37 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
 
-        while (T-- > 0) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken());
-            int M = Integer.parseInt(st.nextToken());
-
-            sb.append(combination(M, N)).append("\n");
+        int N = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            sb.append(combination(y, x)).append("\n");
         }
-        System.out.print(sb);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
     }
 
     private static long combination(int n, int r) {
-        if (r == 0 || n == r) return 1; // 초기 조건
+        if (r == 0 || n == r) {
+            return 1;
+        }
         long[][] dp = new long[n + 1][r + 1];
 
         for (int i = 0; i <= n; i++) {
             for (int j = 0; j <= Math.min(i, r); j++) {
                 if (j == 0 || j == i) {
-                    dp[i][j] = 1; // C(n, 0) = C(n, n) = 1
+                    dp[i][j] = 1;
                 } else {
                     dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
                 }
