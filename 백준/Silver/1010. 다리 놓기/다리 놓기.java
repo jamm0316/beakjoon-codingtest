dc37dfe2ee48600c2d55cc725828b7279188d8fb
evan <168372878+jamm0316@users.noreply.github.com>
2025-01-17 04:46:34 +0900

    [Silver V] Title: 다리 놓기, Time: 120 ms, Memory: 15716 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            sb.append(combination(M, N)).append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    private static long combination(int n, int r) {
+        if (r == 0 || n == r) return 1; // 초기 조건
+        long[][] dp = new long[n + 1][r + 1];
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= Math.min(i, r); j++) {
+                if (j == 0 || j == i) {
+                    dp[i][j] = 1; // C(n, 0) = C(n, n) = 1
+                } else {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][r];
+    }
+}
