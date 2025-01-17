6e709ed10a4fbcebd733875ce45ea21e4cbac790
evan <168372878+jamm0316@users.noreply.github.com>
2025-01-18 06:03:57 +0900

    Create P2748.java

---
+++ DP/브론즈1/2748_피보나치 수열/P2748.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int N = input.nextInt();
+        long[] dp = new long[N + 1];
+        if (1 < N) {
+            dp[0] = 0;
+            dp[1] = 1;
+            for (int i = 2; i < dp.length; i++) {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+            System.out.println(dp[N]);
+        } else {
+            System.out.println(N);
+        }
+    }
+}
