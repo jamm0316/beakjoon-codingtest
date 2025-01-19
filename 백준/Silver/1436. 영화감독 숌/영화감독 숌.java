f293932e1bae3ace64d6f26c6a8ed98ad777a5d5
evan <168372878+jamm0316@users.noreply.github.com>
2025-01-20 08:24:54 +0900

    [Silver V] Title: 영화감독 숌, Time: 268 ms, Memory: 87476 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -1,9 +1,9 @@
-import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int N = input.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
         int count = 1;
         int series = 666;
 
