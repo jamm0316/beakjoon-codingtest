9106878d3ef80738277e1e80c405c916ed1f3e1b
evan <168372878+jamm0316@users.noreply.github.com>
2025-01-20 05:22:07 +0900

    [Silver V] Title: 나이순 정렬, Time: 656 ms, Memory: 50932 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,29 +1,49 @@
 import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    static class Member {
+        int age;
+        String name;
 
-        int N = Integer.parseInt(st.nextToken());
-        List<List<Object>> array = new ArrayList<>();
-
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            List<Object> row = new ArrayList<>();
-            row.add(Integer.parseInt(st.nextToken()));
-            row.add(st.nextToken());
-            array.add(row);
+        public Member(int age, String name) {
+            this.age = age;
+            this.name = name;
         }
 
-        array.sort(Comparator.comparingInt(o -> (Integer) o.get(0)));
-        array.stream().forEach(o -> sb.append(o.get(0)).append(" ").append(o.get(1)).append("\n"));
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
+        public String toString() {
+            return age + " " + name;
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            int N = Integer.parseInt(br.readLine());
+            List<Member> memberList = new ArrayList<>();
+
+            for (int i = 0; i < N; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int age = Integer.parseInt(st.nextToken());
+                String name = st.nextToken();
+
+                memberList.add(new Member(age, name));
+            }
+
+            memberList.stream()
+                    .sorted(Comparator.comparingInt(m -> m.age))
+                    .forEach(m -> {
+                        try {
+                            bw.write(m.toString());
+                            bw.newLine();
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+        }
     }
 }
