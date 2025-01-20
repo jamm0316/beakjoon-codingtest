dcabacffb9bdf609789e967c8e0dd304fc04b244
evan <168372878+jamm0316@users.noreply.github.com>
2025-01-21 08:16:45 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -1,16 +1,23 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int N = input.nextInt();
+    public static void main(String[] args) throws IOException {
+        //12345 -> 120 1
+        //12345678910 -> 3628800 2
+        //1~15 ->1307674368000 3
+        //1~20 -> 2432902008176640000 4
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
         int count = 0;
-        int powerOf5 = 5;
-        while (N >= powerOf5){
-            count += N / powerOf5;
-            powerOf5 *= 5;
+        int five = 5;
+        while (N >= five) {
+            count += N / five;
+            five *= 5;
         }
         System.out.println(count);
     }
 }
-
