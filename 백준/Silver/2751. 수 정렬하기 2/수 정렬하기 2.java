8ab5151609f5c3fe9bd2e7c2c435f015c5b8d685
evan <168372878+jamm0316@users.noreply.github.com>
2025-01-22 08:15:16 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1492 ms, Memory: 159220 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,25 +1,30 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
-        List<Integer> array = new ArrayList<>();
+        List<Integer> sequence = new ArrayList<>();
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            array.add(Integer.parseInt(st.nextToken()));
+            sequence.add(Integer.parseInt(st.nextToken()));
         }
 
-        array.sort(Comparator.naturalOrder());
-        array.stream().forEach(i -> sb.append(i).append("\n"));
-        bw.write(sb.toString());
-        bw.flush();
+        sequence.stream().sorted(Comparator.naturalOrder())
+                .forEach(i -> {
+                    try {
+                        bw.write(String.valueOf(i));
+                        bw.newLine();
+                    } catch (IOException e) {
+
+                    }
+                });
+
         bw.close();
         br.close();
     }
