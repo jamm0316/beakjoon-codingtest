497592ae84f7e5b04e2b53d999299e8e6f6f10f9
evan <168372878+jamm0316@users.noreply.github.com>
2025-02-07 08:21:31 +0900

    [Silver IV] Title: 카드2, Time: 160 ms, Memory: 45456 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -4,23 +4,20 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
-        Deque<Integer> cards = new LinkedList<>();
+        Queue<Integer> queue = new LinkedList<>();
 
         for (int i = 1; i <= N; i++) {
-            cards.add(i);
+            queue.offer(i);
         }
 
-        while (cards.size() > 1) {
-            cards.poll();
-            Integer topCard = cards.poll();
-            cards.add(topCard);
+        //하나를 버리고 제일 위에 카드를 아래로 옮긴다
+        while (queue.size() > 1) {
+            queue.poll();
+            queue.offer(queue.poll());
         }
-        bw.write(String.valueOf(cards.getFirst()));
-        bw.close();
-        br.close();
+        System.out.println(queue.poll());
     }
 }
