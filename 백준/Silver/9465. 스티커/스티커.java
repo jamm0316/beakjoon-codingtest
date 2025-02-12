9df8f5d1d9ac5d5c0f72a2f4fb03b243a1ae33f2
evan <168372878+jamm0316@users.noreply.github.com>
2025-02-13 05:23:51 +0900

    [Silver I] Title: 스티커, Time: 572 ms, Memory: 135464 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -1,39 +1,41 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int n;
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        
-        int T = Integer.parseInt(br.readLine()); // 테스트 케이스 수
-        
-        while (T-- > 0) {
-            int n = Integer.parseInt(br.readLine()); // 스티커 열의 개수
-            int[][] sticker = new int[2][n + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
+        for (int t = 0; t < T; t++) {
+            st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+
             int[][] dp = new int[2][n + 1];
+            int[][] stickerBoard = createStickerBoard(n);
+            dp[0][1] = stickerBoard[0][1];
+            dp[1][1] = stickerBoard[1][1];
+            //System.out.println("stickerBoard = " + Arrays.deepToString(stickerBoard));
 
-            for (int i = 0; i < 2; i++) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                for (int j = 1; j <= n; j++) {
-                    sticker[i][j] = Integer.parseInt(st.nextToken());
-                }
-            }
-
-            // 초기 값 설정
-            dp[0][1] = sticker[0][1];
-            dp[1][1] = sticker[1][1];
-
-            // DP 점화식 적용
             for (int j = 2; j <= n; j++) {
-                dp[0][j] = sticker[0][j] + Math.max(dp[1][j - 1], dp[1][j - 2]);
-                dp[1][j] = sticker[1][j] + Math.max(dp[0][j - 1], dp[0][j - 2]);
+                dp[0][j] = stickerBoard[0][j] + Math.max(dp[1][j - 1], dp[1][j - 2]);
+                dp[1][j] = stickerBoard[1][j] + Math.max(dp[0][j - 1], dp[0][j - 2]);
             }
 
-            // 두 경우 중 최대 값 선택
-            sb.append(Math.max(dp[0][n], dp[1][n])).append("\n");
+            bw.write(String.valueOf(Math.max(dp[0][n], dp[1][n])));
+            bw.newLine();
         }
-        
-        System.out.print(sb);
+        bw.close();
+        br.close();
+    }
+
+    private static int[][] createStickerBoard(int n) throws IOException {
+        int[][] stickerBoard = new int[2][n + 1];
+        for (int i = 0; i < 2; i++) {
+            int[] temp = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            System.arraycopy(temp, 0, stickerBoard[i], 1, n);
+        }
+        return stickerBoard;
     }
 }
