d18194d292a14f66e6104d0c22eca7cef8f983fc
evan <168372878+jamm0316@users.noreply.github.com>
2025-02-15 05:21:06 +0900

    [Gold V] Title: LCS, Time: 136 ms, Memory: 18596 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String A = br.readLine();
+        String B = br.readLine();
+
+        int result = findLCSLength(A, B);
+        System.out.println(result);
+
+    }
+
+    private static int findLCSLength(String A, String B) {
+        int lenA = A.length();
+        int lenB = B.length();
+
+        int[][] dp = new int[lenA + 1][lenB + 1];
+
+        for (int i = 1; i <= lenA; i++) {
+            for (int j = 1; j <= lenB; j++) {
+                if (A.charAt(i - 1) == B.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[lenA][lenB];
+    }
+}
