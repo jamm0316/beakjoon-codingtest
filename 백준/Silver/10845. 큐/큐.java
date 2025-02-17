f006a0bbf783d7a81ff39480522accb81f5b3b12
evan <168372878+jamm0316@users.noreply.github.com>
2025-02-18 08:49:43 +0900

    [Silver IV] Title: 큐, Time: 168 ms, Memory: 19256 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,23 +1,77 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static Queue<Integer> queue = new LinkedList<>();
+    static class PersonalQueue {
+        static Deque<Integer> dequeue = new ArrayDeque<>();
 
+        public void push(int x) {
+            dequeue.offer(x);
+        }
+
+        public int pop() {
+            if (dequeue.size() == 0) {
+                return -1;
+            } else {
+                return dequeue.poll();
+            }
+        }
+
+        public int size() {
+            return dequeue.size();
+        }
+
+        public int empty() {
+            if (dequeue.isEmpty()) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+
+        public int front() {
+            if (dequeue.size() == 0) {
+                return -1;
+            } else {
+                return dequeue.getFirst();
+            }
+        }
+
+        public int back() {
+            if (dequeue.size() == 0) {
+                return -1;
+            } else {
+                return dequeue.getLast();
+            }
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        PersonalQueue deque = new PersonalQueue();
         int N = Integer.parseInt(st.nextToken());
-
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            String cmd = st.nextToken();
-            int inputInt = st.hasMoreTokens() ? Integer.parseInt(st.nextToken()) : -1;
-            Integer result = runQueueMethod(cmd, inputInt);
-            if (result != null) {
-                bw.write(String.valueOf(result));
+            String method = st.nextToken();
+            if (method.equals("push")) {
+                int x = Integer.parseInt(st.nextToken());
+                deque.push(x);
+            } else if (method.equals("pop")) {
+                bw.write(parseStr(deque.pop()));
+                bw.newLine();
+            } else if (method.equals("size")) {
+                bw.write(parseStr(deque.size()));
+                bw.newLine();
+            } else if (method.equals("empty")) {
+                bw.write(parseStr(deque.empty()));
+                bw.newLine();
+            } else if (method.equals("front")) {
+                bw.write(parseStr(deque.front()));
+                bw.newLine();
+            } else if (method.equals("back")) {
+                bw.write(parseStr(deque.back()));
                 bw.newLine();
             }
         }
@@ -25,23 +79,7 @@ public class Main {
         br.close();
     }
 
-    private static Integer runQueueMethod(String cmd, int inputInt) {
-        switch (cmd) {
-            case "push":
-                queue.add(inputInt);
-                return null;
-            case "pop":
-                return queue.isEmpty() ? -1 : queue.poll();
-            case "size":
-                return queue.size();
-            case "empty":
-                return queue.isEmpty() ? 1 : 0;
-            case "front":
-                return queue.isEmpty() ? -1 : queue.peek();
-            case "back":
-                return queue.isEmpty() ? -1 : ((LinkedList<Integer>) queue).peekLast();
-            default:
-                return null;
-        }
+    static private String parseStr(int x) {
+        return String.valueOf(x);
     }
 }
