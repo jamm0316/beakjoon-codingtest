a5e1a5528966f3b448ef1612d79550a46f535e0f
evan <168372878+jamm0316@users.noreply.github.com>
2025-03-02 03:44:55 +0900

    [Silver III] Title: 프린터 큐, Time: 128 ms, Memory: 14812 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -1,40 +1,48 @@
+import java.io.*;
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int testCases = sc.nextInt();
+public class Main  {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        while (testCases-- > 0) {
-            int n = sc.nextInt();  // 문서 개수
-            int m = sc.nextInt();  // 타겟 문서의 위치
+        int testCase = Integer.parseInt(st.nextToken());
+
+        while (testCase-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());  //문서의 개수
+            int M = Integer.parseInt(st.nextToken());  //타겟 문서의 위치
 
             Queue<int[]> queue = new LinkedList<>();
             PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
 
-            for (int i = 0; i < n; i++) {
-                int priority = sc.nextInt();
-                queue.add(new int[]{priority, i});  // {중요도, 초기 인덱스}
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                int priority = Integer.parseInt(st.nextToken());
+                queue.add(new int[]{priority, i});  //우선순위 및 초기 인덱스
                 priorityQueue.add(priority);
             }
 
-            int printOrder = 0;  // 출력 순서 카운트
+            int printOrder = 0;  //출력순서 카운트
 
             while (!queue.isEmpty()) {
                 int[] current = queue.poll();
-                if (current[0] == priorityQueue.peek()) { // 현재 문서가 가장 높은 중요도라면
+                if (current[0] == priorityQueue.peek()) {  //현재문서가 가장 높은 중요도라면
                     priorityQueue.poll();
                     printOrder++;
 
-                    if (current[1] == m) { // 찾고자 하는 문서면 결과 출력
-                        System.out.println(printOrder);
+                    if (current[1] == M) {  //찾고자 하는 문서면 결과 출력
+                        bw.write(String.valueOf(printOrder));
+                        bw.newLine();
                         break;
                     }
-                } else { // 중요도가 더 높은 문서가 있다면 뒤로 보냄
+                } else {
                     queue.add(current);
                 }
             }
         }
-        sc.close();
+        bw.close();
+        br.close();
     }
 }
