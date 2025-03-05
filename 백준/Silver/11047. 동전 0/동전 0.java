78a0a7deb8ec476defaaff4f06b2c4edd6047a8b
evan <168372878+jamm0316@users.noreply.github.com>
2025-03-06 08:24:48 +0900

    [Silver IV] Title: 동전 0, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,5 +1,5 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -8,19 +8,19 @@ public class Main {
 
         int N = Integer.parseInt(st.nextToken());
         int target = Integer.parseInt(st.nextToken());
-        int[] coins = new int[N];
         int count = 0;
+        List<Integer> coins = new ArrayList<>();
+
         for (int i = 0; i < N; i++) {
-            coins[i] = Integer.parseInt(br.readLine());
+            coins.add(Integer.parseInt(br.readLine()));
         }
 
-        for (int i = N - 1; i >= 0; i--) {
-            if (target == 0) {
-                break;
-            }
-            if (coins[i] <= target) {
-                count += target / coins[i];
-                target %= coins[i];
+        coins.sort(Comparator.reverseOrder());
+
+        for (int curCoin : coins) {
+            if (curCoin <= target) {
+                count += target / curCoin;
+                target %= curCoin;
             }
         }
 
