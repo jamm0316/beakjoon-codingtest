209db44144ffb2f2b19f5271ca0181504ca0841c
evan <168372878+jamm0316@users.noreply.github.com>
2025-03-27 05:42:01 +0900

    Update P11727.java

--- DP/실버3/11727_2×n 타일링 2/P11727.java
+++ DP/실버3/11727_2×n 타일링 2/P11727.java
@@ -7,12 +7,13 @@ public class Main {
         int[] dp = new int[n + 1];
 
         dp[1] = 1;
-        dp[2] = 3;
 
-        if (n > 2) {
-            for (int i = 3; i <= n; i++) {
-                dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10_007;
-            }
+        if (n >= 2) {
+            dp[2] = 3;
+        }
+        
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10_007;
         }
         System.out.println(dp[n]);
     }
