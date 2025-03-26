4a1cd3d964d56e25ff673022a4d90728bd532253
evan <168372878+jamm0316@users.noreply.github.com>
2025-03-27 01:08:31 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 108 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testCase = Integer.parseInt(br.readLine());
+
+        while (testCase-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            Map<String, Integer> map = new HashMap<>();
+
+            for (int i = 0; i < n; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                String name = st.nextToken();
+                String type = st.nextToken();
+
+                map.put(type, map.getOrDefault(type, 0) + 1);
+            }
+
+            int result = 1;
+            for (int count : map.values()) {
+                result *= (count + 1);
+            }
+
+            System.out.println(result - 1);
+        }
+    }
+}
