8a56dfe3422c525d5fee3d33e45c6c39d4b44301
evan <evanbackeng@gmail.com>
2025-04-03 05:47:27 +0900

    [Silver II] Title: 좌표 압축, Time: 2324 ms, Memory: 356400 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,30 +1,35 @@
 import java.io.*;
 import java.util.*;
+import java.util.stream.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        StringBuilder sb = new StringBuilder();
+        
+        //input 초기화
         int N = Integer.parseInt(br.readLine());
-        int[] basePosition = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        Set<Integer> uniquePositionSet = new HashSet<>();
-        for (int eachPosition : basePosition) {
-            uniquePositionSet.add(eachPosition);
-        }
+        int[] positions = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-        List<Integer> sortedPositionList = new ArrayList<>(uniquePositionSet);
+        //Set 초기화
+        Set<Integer> uniqueSet = Arrays.stream(positions).boxed().collect(Collectors.toSet());
+
+        //List 초기화 및 정렬
+        List<Integer> sortedPositionList = new ArrayList<>(uniqueSet);
         sortedPositionList.sort(Comparator.naturalOrder());
 
-        Map<Integer, Integer> compressPositionMap = new HashMap<>();
+        //Map 초기화
+        Map<Integer, Integer> compressMap = new HashMap<>();
         for (int i = 0; i < sortedPositionList.size(); i++) {
-            compressPositionMap.put(sortedPositionList.get(i), i);
+            compressMap.put(sortedPositionList.get(i), i);
         }
 
-        for (int eachPosition : basePosition) {
-            bw.write(compressPositionMap.get(eachPosition) + " ");
+        //결과값 출력
+        for (int eachPosition : positions) {
+            sb.append(compressMap.get(eachPosition) + " ");
         }
-
+        bw.write(sb.toString());
         bw.close();
         br.close();
     }
