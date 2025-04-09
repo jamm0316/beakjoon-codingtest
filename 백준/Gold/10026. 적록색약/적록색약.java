51d1dd8c9ca280225098a5dc15929ccfbdc8572e
evan <evanbackeng@gmail.com>
2025-04-10 06:28:53 +0900

    [Gold V] Title: 적록색약, Time: 144 ms, Memory: 17152 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -5,87 +5,61 @@ public class Main {
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
     static int N;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         N = Integer.parseInt(br.readLine());
-        String[][] normalGraph = new String[N][N];
-        String[][] blindnessGraph = new String[N][N];
-        boolean[][] normalVisited = new boolean[N][N];
-        boolean[][] blindnessVisited = new boolean[N][N];
-        int normalCount = 0;
-        int blindnessCount = 0;
 
-        //그래프 초기화
+        char[][] normalGraph = new char[N][N];
+        char[][] blindnessGraph = new char[N][N];
+
         for (int i = 0; i < N; i++) {
-            String[] tokens = br.readLine().split("");
+            String line = br.readLine();
             for (int j = 0; j < N; j++) {
-                String color = tokens[j];
+                char color = line.charAt(j);
                 normalGraph[i][j] = color;
-                blindnessGraph[i][j] = color.equals("R") ? "G" : color;
+                blindnessGraph[i][j] = (color == 'R') ? 'G' : color;
             }
         }
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                //일반인 탐색
-                if (!normalVisited[i][j]) {
-                    countColorBoundary(normalGraph, normalVisited, i, j);
-                    normalCount++;
+        int normalCount = countRegions(normalGraph);
+        int blindnessCount = countRegions(blindnessGraph);
 
-//                    //debug
-//                    System.out.println("nomalCount: " + normalCount + "--------------");
-//                    System.out.println("-------[graph]-------");
-//                    for (int row = 0; row < N; row++) {
-//                        System.out.println(Arrays.toString(normalGraph[row]));
-//                    }
-//                    System.out.println("-------[visited]-------");
-//                    for (int row = 0; row < N; row++) {
-//                        System.out.println(Arrays.toString(normalVisited[row]));
-//                    }
-                }
-
-                //색약탐색
-                if (!blindnessVisited[i][j]) {
-                    countColorBoundary(blindnessGraph, blindnessVisited, i, j);
-                    blindnessCount++;
-
-//                    //debug
-//                    System.out.println("blindnessCount: " + blindnessCount + "--------------");
-//                    System.out.println("-------[graph]-------");
-//                    for (int row = 0; row < N; row++) {
-//                        System.out.println(Arrays.toString(blindnessGraph[row]));
-//                    }
-//                    System.out.println("-------[visited]-------");
-//                    for (int row = 0; row < N; row++) {
-//                        System.out.println(Arrays.toString(blindnessVisited[row]));
-//                    }
-                }
-            }
-        }
-
-        bw.write(normalCount + " " + blindnessCount);
-        bw.close();
-        br.close();
+        System.out.println(normalCount + " " + blindnessCount);
     }
 
-    static private void countColorBoundary(String[][] graph, boolean[][] visited, int i, int j) {
+    static int countRegions(char[][] graph) {
+        boolean[][] visited = new boolean[N][N];
+        int count = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!visited[i][j]) {
+                    bfs(graph, visited, i, j);
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    static void bfs(char[][] graph, boolean[][] visited, int i, int j) {
         Queue<int[]> queue = new LinkedList<>();
         queue.offer(new int[]{i, j});
         visited[i][j] = true;
+        char color = graph[i][j];
 
         while (!queue.isEmpty()) {
             int[] pos = queue.poll();
             int x = pos[0], y = pos[1];
-            String curColor = graph[x][y];
+
             for (int k = 0; k < 4; k++) {
                 int nx = x + dx[k];
                 int ny = y + dy[k];
 
                 if (0 <= nx && nx < N && 0 <= ny && ny < N && !visited[nx][ny]) {
-                    String nextColor = graph[nx][ny];
-                    if (curColor.equals(nextColor)) {
+                    if (graph[nx][ny] == color) {
                         queue.offer(new int[]{nx, ny});
                         visited[nx][ny] = true;
                     }
