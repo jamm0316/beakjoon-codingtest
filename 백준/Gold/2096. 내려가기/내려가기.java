2e1e178dd3f2395ccf8ac051ed41ac9257f05ef5
evan <evanbackeng@gmail.com>
2025-04-18 05:29:21 +0900

    [Gold V] Title: 내려가기, Time: 544 ms, Memory: 74004 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -4,42 +4,32 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int N = Integer.parseInt(br.readLine());
-        int[][] graph = new int[N][3];
-        int[][] maxDp = new int[N][3];
-        int[][] minDp = new int[N][3];
-        for (int i = 0; i < N; i++) {
-            graph[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
+        int[][] maxDp = new int[2][3];
+        int[][] minDp = new int[2][3];
+
+        int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        System.arraycopy(input, 0, maxDp[0], 0, 3);
+        System.arraycopy(input, 0, minDp[0], 0, 3);
+
+        for (int i = 1; i < N; i++) {
+            input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int cur = i % 2, prev = (i - 1) % 2;
+
+            maxDp[cur][0] = Math.max(maxDp[prev][0], maxDp[prev][1]) + input[0];
+            maxDp[cur][1] = Math.max(Math.max(maxDp[prev][0], maxDp[prev][1]), maxDp[prev][2]) + input[1];
+            maxDp[cur][2] = Math.max(maxDp[prev][1], maxDp[prev][2]) + input[2];
+
+            minDp[cur][0] = Math.min(minDp[prev][0], minDp[prev][1]) + input[0];
+            minDp[cur][1] = Math.min(Math.min(minDp[prev][0], minDp[prev][1]), minDp[prev][2]) + input[1];
+            minDp[cur][2] = Math.min(minDp[prev][1], minDp[prev][2]) + input[2];
         }
 
-        for (int i = 0; i < N; i++) {
-            if (i == 0) {
-                maxDp[i] = graph[i];
-                minDp[i] = graph[i];
-            } else {
-                for (int j = 0; j < 3; j++) {
-                    if (j == 0) {
-                        maxDp[i][j] = Math.max(maxDp[i - 1][j] + graph[i][j], maxDp[i - 1][j + 1] + graph[i][j]);
-                        minDp[i][j] = Math.min(minDp[i - 1][j] + graph[i][j], minDp[i - 1][j + 1] + graph[i][j]);
-                    } else if (j == 1) {
-                        maxDp[i][j] = Math.max(maxDp[i - 1][j] + graph[i][j], maxDp[i - 1][j - 1] + graph[i][j]);
-                        maxDp[i][j] = Math.max(maxDp[i][j], maxDp[i - 1][j + 1] + graph[i][j]);
-                        minDp[i][j] = Math.min(minDp[i - 1][j] + graph[i][j], minDp[i - 1][j - 1] + graph[i][j]);
-                        minDp[i][j] = Math.min(minDp[i][j], minDp[i - 1][j + 1] + graph[i][j]);
-                    } else {
-                        maxDp[i][j] = Math.max(maxDp[i - 1][j] + graph[i][j], maxDp[i - 1][j - 1] + graph[i][j]);
-                        minDp[i][j] = Math.min(minDp[i - 1][j] + graph[i][j], minDp[i - 1][j - 1] + graph[i][j]);
-                    }
-                }
-            }
-        }
-        int max = Arrays.stream(maxDp[N - 1]).max().getAsInt();
-        int min = Arrays.stream(minDp[N - 1]).min().getAsInt();
-        bw.write(max + " " + min);
-        bw.close();
-        br.close();
+        int last = (N - 1) % 2;
+        int max = Arrays.stream(maxDp[last]).max().getAsInt();
+        int min = Arrays.stream(minDp[last]).min().getAsInt();
+
+        System.out.println(max + " " + min);
     }
 }
