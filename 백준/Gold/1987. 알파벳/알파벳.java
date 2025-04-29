b842aaa579575fecdc37947f45dccdf87e25c5f5
evan <evanbackeng@gmail.com>
2025-04-30 05:30:40 +0900

    [Gold IV] Title: 알파벳, Time: 1176 ms, Memory: 295228 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -3,69 +3,66 @@ import java.util.*;
 
 public class Main {
     static class Node {
-        StringBuilder sb = new StringBuilder();
         int x;
         int y;
-        int dist;
+        int count;
 
-        Node(int x, int y, int dist) {
+        Node(int x, int y, int count) {
             this.x = x;
             this.y = y;
-            this.dist = dist;
+            this.count = count;
         }
 
         @Override
         public String toString() {
-            sb.append("Node{pos: [").append(x).append(", ").append(y)
-                    .append("], dist: ").append(dist).append("}").append('\n')
-                    .append(visited);
-            return sb.toString();
+            return "Node{x: " + x + ", y: " + y + ", count: " + count + "}";
         }
     }
-
-    static int R, C;
-    static char[][] graph;
-    static boolean[] visited = new boolean[26];
-    static int maxDist = 0;
 
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
 
+    static int N, M;
+    static boolean[] visited;
+    static char[][] graph;
+    static int maxCount;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        visited = new boolean[26];
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        R = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-
-        //그래프 초기화
-        graph = new char[R][C];
-        for (int i = 0; i < R; i++) {
+        graph = new char[N][M];
+        for (int i = 0; i < N; i++) {
             graph[i] = br.readLine().toCharArray();
         }
 
-        //DFS탐색
         visited[graph[0][0] - 'A'] = true;
-        recursive(new Node(0, 0, 1));
-
-        System.out.println(maxDist);
+        backtrack(new Node(0, 0, 1));
+        System.out.println(maxCount);
     }
 
-    private static void recursive(Node now) {
-        maxDist = Math.max(maxDist, now.dist);
+    static void backtrack(Node node) {
+        maxCount = Math.max(maxCount, node.count);
+
+        visited[graph[node.x][node.y] - 'A'] = true;
 
         for (int i = 0; i < 4; i++) {
-            int nx = now.x + dx[i];
-            int ny = now.y + dy[i];
+            int nx = node.x + dx[i];
+            int ny = node.y + dy[i];
 
-            if (0 <= nx && nx < R && 0 <= ny && ny < C) {
-                int idx = graph[nx][ny] - 'A';
-                if (!visited[idx]) {
-                    visited[idx] = true;
-                    recursive(new Node(nx, ny, now.dist + 1));
-                    visited[idx] = false;
-                }
+            if (0 <= nx && nx < N && 0 <= ny && ny < M
+                    && !isVisited(nx, ny)) {
+                visited[graph[nx][ny] - 'A'] = true;
+                backtrack(new Node(nx, ny, node.count + 1));
+                visited[graph[nx][ny] - 'A'] = false;
             }
         }
+    }
+
+    static boolean isVisited(int x, int y) {
+        return visited[graph[x][y] - 'A'];
     }
 }
