f9b18bb26dcfae97e77910d6ed28d749485940d9
evan <evanbackeng@gmail.com>
2025-05-06 05:55:23 +0900

    [Gold II] Title: 피보나치 수 6, Time: 104 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.java
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.java
@@ -1,67 +1,47 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
- 
+
 public class Main {
-	
-	final static long MOD = 1000000007;
-	public static long[][] origin = {{1, 1} , {1, 0}};	// 초기값을 갖고있는 행렬
-	
-	public static void main(String[] args) throws IOException{
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		/*
-		 *                n
-		 *       | 1   1 |    | F(n+1)  F(n)  |
-		 * A^n = |       |  = |               |
-		 *       | 1   0 |    |  F(n)  F(n-1) |
-		 */
-		
-		long[][] A = {{1, 1} , {1, 0}};
-		
-		
-		long N = Long.parseLong(br.readLine());
-		
-		
-		// Fn 을 구하려면  A행렬을 n-1제곱 한 뒤 반환된 행렬 A11 원소를 출력하면 된다.
-		
-		System.out.println(pow(A, N - 1)[0][0]);
-	}
-	
-	// 행렬 제곱 분할정복 메소드
-	public static long[][] pow(long[][] A, long exp) {
- 
-		// 지수가 1 또는 0일 땐 A를 return한다.
-		if(exp == 1 || exp == 0) {
-			return A;
-		}
-		
-		// 지수를 절반으로 분할하여 재귀호출
-		long[][] ret = pow(A, exp / 2);
-		
-		// 하위 재귀에서 얻은 행렬을 제곱해준다.
-		ret = multiply(ret, ret);
-		
-		// 만약 지수가 홀수라면 마지막에  A^1 (origin)을 곱해준다.
-		if(exp % 2 == 1L) {
-			ret = multiply(ret, origin);
-		}
-		
-		return ret;
-	}
-	
-	
-	// o1과 o2 행렬을 곱해주는 메소드
-	public static long[][] multiply(long[][] o1, long[][] o2) {
-		
-		long[][] ret = new long[2][2];
- 
-		ret[0][0] = ((o1[0][0] * o2[0][0]) + (o1[0][1] * o2[1][0])) % MOD;
-		ret[0][1] = ((o1[0][0] * o2[0][1]) + (o1[0][1] * o2[1][1])) % MOD;
-		ret[1][0] = ((o1[1][0] * o2[0][0]) + (o1[1][1] * o2[1][0])) % MOD;
-		ret[1][1] = ((o1[1][0] * o2[0][1]) + (o1[1][1] * o2[1][1])) % MOD;
-		return ret;
-	}
- 
+
+    static final long MOD = 1_000_000_007;
+    static final long[][] ORIGIN = {{1, 1}, {1, 0}};  // 상수 행렬
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long N = Long.parseLong(br.readLine());
+
+        // F(0)은 0이므로 예외 처리
+        if (N == 0) {
+            System.out.println(0);
+            return;
+        }
+
+        long[][] result = pow(ORIGIN, N - 1);
+        System.out.println(result[0][0]);  // F(N)
+    }
+
+    // 행렬을 거듭제곱하는 메서드 (분할 정복)
+    public static long[][] pow(long[][] A, long exp) {
+        if (exp == 1 || exp == 0) return A;
+
+        long[][] half = pow(A, exp / 2);
+        long[][] result = multiply(half, half);
+
+        if (exp % 2 == 1) {
+            result = multiply(result, ORIGIN);
+        }
+
+        return result;
+    }
+
+    // 2x2 행렬 곱셈
+    public static long[][] multiply(long[][] a, long[][] b) {
+        long[][] res = new long[2][2];
+        res[0][0] = (a[0][0] * b[0][0] + a[0][1] * b[1][0]) % MOD;
+        res[0][1] = (a[0][0] * b[0][1] + a[0][1] * b[1][1]) % MOD;
+        res[1][0] = (a[1][0] * b[0][0] + a[1][1] * b[1][0]) % MOD;
+        res[1][1] = (a[1][0] * b[0][1] + a[1][1] * b[1][1]) % MOD;
+        return res;
+    }
 }
