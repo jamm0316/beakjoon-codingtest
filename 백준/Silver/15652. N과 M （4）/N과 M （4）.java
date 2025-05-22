4a516131a18f517ca1cdfcdafae01b055badff4d
evan <evanbackeng@gmail.com>
2025-05-23 08:44:43 +0900

    [Silver III] Title: N과 M (4), Time: 152 ms, Memory: 17348 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        dfs(1, new ArrayList<>());
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
+    }
+
+    private static void dfs(int start, List<Integer> list) {
+        if (list.size() == M) {
+            for (int i : list) {
+                sb.append(i).append(" ");
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = start; i <= N; i++) {
+            list.add(i);
+            dfs(i, list);
+            list.remove(list.size() - 1);
+        }
+    }
+}
