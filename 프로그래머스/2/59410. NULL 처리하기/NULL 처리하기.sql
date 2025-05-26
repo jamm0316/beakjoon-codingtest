c094f187c48a886a49f662ee7b47cfaf9a742eb6
evan <evanbackeng@gmail.com>
2025-05-26 15:43:01 +0900

    [level 2] Title: NULL 처리하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -1,6 +1,4 @@
 -- 코드를 입력하세요
-SELECT ANIMAL_TYPE,
-coalesce (name, 'No name') as name,
-SEX_UPON_INTAKE
+select ANIMAL_TYPE, coalesce(NAME, "No name") as NAME, SEX_UPON_INTAKE
 from ANIMAL_INS
 order by ANIMAL_ID;
