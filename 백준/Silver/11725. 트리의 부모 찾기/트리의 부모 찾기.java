4e4dc0144256883cefcf62607240fd8d4961b10b
evan <evanbackeng@gmail.com>
2025-05-27 05:27:32 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 520 ms, Memory: 66472 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,47 +1,38 @@
+/**
+ * 1. parent[] dp를 만들어 해당 노드들의 부모를 찾아낸다.
+ * 2. tree는 양방향으로 만든다.
+ * 3. bfs를 이용해 root를 1로 주고, !visited인 노드를 찾아 다니면서 자식노드의 dp[index]에 부모 노드를 넣어준다.*/
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
     static StringBuilder sb = new StringBuilder();
     static int N;
-    static List<List<Integer>> tree;
+    static List<List<Integer>> tree = new ArrayList<>();
     static boolean[] visited;
     static int[] parent;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         N = Integer.parseInt(br.readLine());
-        tree = new ArrayList<>(N + 1);
         visited = new boolean[N + 1];
         parent = new int[N + 1];
+
         for (int i = 0; i <= N; i++) {
             tree.add(new ArrayList<>());
         }
 
-        for (int i = 1; i < N; i++) {
+        for (int i = 0; i < N - 1; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-
             tree.get(start).add(end);
             tree.get(end).add(start);
         }
-        bfs(1);
-
-        for (int i = 2; i <= N; i++) {
-            sb.append(parent[i]).append('\n');
-        }
-        bw.write(sb.toString());
-        bw.close();
-        br.close();
-    }
-
-    private static void bfs(int root) {
         Queue<Integer> queue = new LinkedList<>();
-        queue.offer(root);
-        visited[root] = true;
+        queue.offer(1);
+        visited[1] = true;
 
         while (!queue.isEmpty()) {
             int now = queue.poll();
@@ -53,5 +44,13 @@ public class Main {
                 }
             }
         }
+
+        for (int i = 2; i <= N; i++) {
+            sb.append(parent[i]).append('\n');
+        }
+
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
     }
 }
