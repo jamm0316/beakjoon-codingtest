a60d97270f5c2e9696d3db8937ccf89898a604a1
evan <evanbackeng@gmail.com>
2025-05-28 11:58:20 +0900

    [level 2] Title: 피로도, Time: 5.66 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,25 +1,30 @@
+/*
+1. 완전 탐색, dfs, 백트래킹
+*/
 import java.util.*;
 
 class Solution {
-    static int answer = 0;
-    static int N;
-    public int solution(int k, int[][] dungeons) {
+    static int N, count, answer;
+    static int[][] dungeons;
+    static boolean[] visited;
+    public int solution(int k, int[][] input) {
+        dungeons = input;
         N = dungeons.length;
-        boolean[] visited = new boolean[N];
-        dfs(k, dungeons, visited, 0);
+        visited = new boolean[N];
+        dfs(k, 0);
         return answer;
     }
     
-    static private void dfs(int tired, int[][] dungeons, boolean[] visited, int count) {
+    static private void dfs(int tired, int count) {
         answer = Math.max(answer, count);
         
-        for (int i = 0; i < N; i++) {
+        for (int i =0; i < N; i++) {
             int required = dungeons[i][0];
             int cost = dungeons[i][1];
-            
-            if (!visited[i] && tired >= required) {
+            if(!visited[i] && tired >= required){
                 visited[i] = true;
-                dfs(tired - cost, dungeons, visited, count + 1);
+                
+                dfs(tired-cost, count + 1);
                 visited[i] = false;
             }
         }
