559c586ae78f751b30c4607e87402e417829ad85
evan <evanbackeng@gmail.com>
2025-06-05 06:19:45 +0900

    [level 3] Title: 등굣길, Time: 1.08 ms, Memory: 54.3 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,35 +1,33 @@
+/*
+dp[i][j] = row[i - 1] + col[i - 1]
+*/
+
 import java.util.*;
 
 class Solution {
-    static int row, col;
-    static int[][] dp;
     static int MOD = 1_000_000_007;
-    
     public int solution(int m, int n, int[][] puddles) {
-        row = n;
-        col = m;
-        dp = new int[row][col];
-        
+        int row = n;
+        int col = m;
+        int[][] dp = new int[row][col];
         for (int[] puddle : puddles) {
             dp[puddle[1] - 1][puddle[0] - 1] = -1;
         }
         
         dp[0][0] = 1;
         
-        for (int x = 0; x < row; x++) {
-            for (int y = 0; y < col; y++) {
-                if (dp[x][y] == -1) {
-                    dp[x][y] = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (dp[i][j] == -1) {
+                    dp[i][j] = 0;
                     continue;
                 }
-                
-                if (x > 0) dp[x][y] += dp[x - 1][y] % MOD;
-                if (y > 0) dp[x][y] += dp[x][y - 1] % MOD;
-                
-                dp[x][y] %= MOD;
+                if (0 < i) dp[i][j] += dp[i - 1][j] % MOD;
+                if (0 < j) dp[i][j] += dp[i][j - 1] % MOD;
+                dp[i][j] %= MOD;
             }
         }
         
-        return dp[row - 1][col - 1];
+        return dp[n - 1][m - 1];
     }
 }
