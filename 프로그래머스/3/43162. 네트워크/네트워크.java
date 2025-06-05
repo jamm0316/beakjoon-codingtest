426e7adeab61b4a7e688be3400e5431d936a7a0a
evan <evanbackeng@gmail.com>
2025-06-05 09:01:21 +0900

    [level 3] Title: 네트워크, Time: 0.20 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,33 +1,27 @@
 /*
-1. dfs로 모든 노드 확인
-2. 순회할 때 visited를 기준으로 있으면 dfs 없으면 dfs 안함
-3. network++ 실시
+
 */
 import java.util.*;
 
 class Solution {
-    static int N, network;
-    static int[][] computers;
     static boolean[] visited;
-    public int solution(int n, int[][] input) {
-        N = n;
-        computers = input;
-        visited = new boolean[N];
-        
-        for (int i = 0 ; i < N; i++) {
+    static int result;
+    public int solution(int n, int[][] computers) {
+        visited = new boolean[n];
+        for (int i = 0; i < n; i++) {
             if (!visited[i]) {
-                dfs(i);
-                network++;
+                dfs(i, computers);
+                result++;
             }
         }
-        return network;
+        return result;
     }
     
-    static private void dfs (int start) {
-        for (int i=0; i < N; i++) {
+    private void dfs(int start, int[][] computers) {
+        visited[start] = true;
+        for (int i = 0; i < computers[start].length; i++) {
             if (!visited[i] && computers[start][i] == 1) {
-                visited[i] = true;
-                dfs(i);
+                dfs(i, computers);
             }
         }
     }
