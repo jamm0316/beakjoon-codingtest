1008342c63d0b3386c1d85a6126f894257293a5b
evan <evanbackeng@gmail.com>
2025-06-10 05:40:35 +0900

    [Silver I] Title: 정수 삼각형, Time: 280 ms, Memory: 31160 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -4,39 +4,24 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
         List<List<Integer>> tree = new ArrayList<>();
+        int N = Integer.parseInt(br.readLine());
         for (int i = 0; i < N; i++) {
             tree.add(new ArrayList<>());
-        }
-        tree.get(0).add(Integer.parseInt(br.readLine()));
-
-        for (int i = 1; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int idx = 0;
             while (st.hasMoreTokens()) {
-                int cur = Integer.parseInt(st.nextToken());
-                int prev = 0;
-                if (i == 1) {
-                    prev = tree.get(0).get(0);
-                    tree.get(1).add(cur + prev);
-                } else {
-                    if (idx == 0) {
-                        prev = tree.get(i - 1).get(idx);
-                        tree.get(i).add(cur + prev);
-                    } else if (idx == i) {
-                        prev = tree.get(i - 1).get(idx - 1);
-                        tree.get(i).add(cur + prev);
-                    } else {
-                        prev = Math.max(tree.get(i - 1).get(idx - 1), tree.get(i - 1).get(idx));
-                        tree.get(i).add(cur + prev);
-                    }
-                    idx++;
-                }
+                tree.get(i).add(Integer.parseInt(st.nextToken()));
             }
         }
 
-        Integer i = tree.get(N - 1).stream().max(Comparator.naturalOrder()).get();
-        System.out.println(i);
+        for (int i = N - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                int cur = tree.get(i).get(j);
+                int belowLeft = tree.get(i + 1).get(j);
+                int belowRight = tree.get(i + 1).get(j + 1);
+                tree.get(i).set(j, cur + Math.max(belowLeft, belowRight));
+            }
+        }
+        System.out.println(tree.get(0).get(0));
     }
 }
