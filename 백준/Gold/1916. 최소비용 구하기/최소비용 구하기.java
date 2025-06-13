480e17b9155c3af7bbf62c21ae195907611bfeb0
evan <evanbackeng@gmail.com>
2025-06-13 15:16:42 +0900

    [Gold V] Title: 최소비용 구하기, Time: 480 ms, Memory: 51660 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -3,65 +3,55 @@ import java.util.*;
 
 public class Main {
     static class Node {
-        int nextCity, cost;
-        Node (int nextCity, int cost) {
-            this.nextCity = nextCity;
+        int next;
+        int cost;
+
+        Node(int next, int cost) {
+            this.next = next;
             this.cost = cost;
         }
 
         @Override
         public String toString() {
-            final StringBuilder sb = new StringBuilder("Node{");
-            sb.append("nextCity=").append(nextCity);
-            sb.append(", cost=").append(cost);
-            sb.append('}');
-            return sb.toString();
+            return "{next: " + next + ", cost: " + cost + "}";
         }
     }
-
     static StringTokenizer st;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
-        List<List<Node>> graph = new ArrayList<>(N + 1);
+        List<List<Node>> busInfo = new ArrayList<>();
         for (int i = 0; i <= N; i++) {
-            graph.add(new ArrayList<>());
+            busInfo.add(new ArrayList<>());
         }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-            graph.get(from).add(new Node(to, cost));
+            int start = Integer.parseInt(st.nextToken()), end = Integer.parseInt(st.nextToken()),
+                    cost = Integer.parseInt(st.nextToken());
+            busInfo.get(start).add(new Node(end, cost));
         }
 
         st = new StringTokenizer(br.readLine());
-        int start = Integer.parseInt(st.nextToken());
-        int end = Integer.parseInt(st.nextToken());
+        int start = Integer.parseInt(st.nextToken()), end  = Integer.parseInt(st.nextToken());
 
+        int[] dp = new int[N + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        dp[start] = 0;
         Queue<Node> queue = new PriorityQueue<>((a, b) -> a.cost - b.cost);
         queue.offer(new Node(start, 0));
-        int[] dist = new int[N + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[start] = 0;
-
         while (!queue.isEmpty()) {
-            Node curCity = queue.poll();
-
-            //현재 비용이 기존에 저장된 비용보다 더 크면 skip
-            if (curCity.cost > dist[curCity.nextCity]) continue;
-
-
-            for (Node neighbor : graph.get(curCity.nextCity)) {
-                if (dist[neighbor.nextCity] > dist[curCity.nextCity] + neighbor.cost) {
-                    dist[neighbor.nextCity] = dist[curCity.nextCity] + neighbor.cost;
-                    queue.offer(new Node(neighbor.nextCity, dist[neighbor.nextCity]));
+            Node now = queue.poll();
+            if (now.cost > dp[now.next]) continue;
+            for (Node nextNode : busInfo.get(now.next)) {
+                if (dp[nextNode.next] > nextNode.cost + now.cost) {
+                    dp[nextNode.next] = nextNode.cost + now.cost;
+                    queue.offer(new Node(nextNode.next, dp[nextNode.next]));
                 }
             }
         }
-
-        System.out.println(dist[end]);
+        System.out.println(dp[end]);
     }
 }
