3cee6f2f91030c65ba61278cc9c855d155c3da17
evan <evanbackeng@gmail.com>
2025-06-14 06:09:34 +0900

    [Gold V] Title: 내려가기, Time: 328 ms, Memory: 44272 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -4,32 +4,39 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
-        int[][] maxDp = new int[2][3];
-        int[][] minDp = new int[2][3];
-        int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        System.arraycopy(input, 0, maxDp[0], 0, 3);
-        System.arraycopy(input, 0, minDp[0], 0, 3);
+        int[][] dpMax = new int[2][3];
+        int[][] dpMin = new int[2][3];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 3; i++) {
+            int token = Integer.parseInt(st.nextToken());
+            dpMax[0][i] = token;
+            dpMin[0][i] = token;
+        }
 
         for (int i = 1; i < N; i++) {
-            input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            int cur = i % 2, prev = (i - 1) % 2;
-
-            maxDp[cur][0] = Math.max(maxDp[prev][0], maxDp[prev][1]) + input[0];
-            maxDp[cur][1] = Math.max(Math.max(maxDp[prev][0], maxDp[prev][1]), maxDp[prev][2]) + input[1];
-            maxDp[cur][2] = Math.max(maxDp[prev][1], maxDp[prev][2]) + input[2];
-
-            minDp[cur][0] = Math.min(minDp[prev][0], minDp[prev][1]) + input[0];
-            minDp[cur][1] = Math.min(Math.min(minDp[prev][0], minDp[prev][1]), minDp[prev][2]) + input[1];
-            minDp[cur][2] = Math.min(minDp[prev][1], minDp[prev][2]) + input[2];
+            st = new StringTokenizer(br.readLine());
+            int prev = (i + 1) % 2;
+            int now = (prev + 1) % 2;
+            for (int j = 0; j < 3; j++) {
+                int token = Integer.parseInt(st.nextToken());
+                if (j == 0) {
+                    dpMax[now][j] = token + Math.max(dpMax[prev][0], dpMax[prev][1]);
+                    dpMin[now][j] = token + Math.min(dpMin[prev][0], dpMin[prev][1]);
+                } else if (j == 2) {
+                    dpMax[now][j] = token + Math.max(dpMax[prev][1], dpMax[prev][2]);
+                    dpMin[now][j] = token + Math.min(dpMin[prev][1], dpMin[prev][2]);
+                } else {
+                    dpMax[now][j] = token + Math.max(Math.max(dpMax[prev][j - 1], dpMax[prev][j]), dpMax[prev][j + 1]);
+                    dpMin[now][j] = token + Math.min(Math.min(dpMin[prev][j - 1], dpMin[prev][j]), dpMin[prev][j + 1]);
+                }
+            }
         }
-        int last = (N - 1) % 2;
-        int max = Arrays.stream(maxDp[last]).max().getAsInt();
-        int min = Arrays.stream(minDp[last]).min().getAsInt();
-        bw.write(max + " " + min);
-        bw.close();
-        br.close();
+        int max = Arrays.stream(dpMax[(N + 1) % 2]).max().getAsInt();
+        int min = Arrays.stream(dpMin[(N + 1) % 2]).min().getAsInt();
+
+        System.out.println(sb.append(max).append(" ").append(min));
     }
 }
