91e86507527b20a1f39aa035d80ae6fc526a8335
evan <evanbackeng@gmail.com>
2025-06-16 09:00:41 +0900

    [Gold V] Title: LCS, Time: 132 ms, Memory: 18596 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,21 +1,21 @@
 import java.io.*;
 
-public class Main {
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String origin = br.readLine();
         String test = br.readLine();
-
+        
         int result = findLCS(origin, test);
         System.out.println(result);
     }
-
+    
     private static int findLCS(String origin, String test) {
         int N = origin.length();
         int M = test.length();
-
+        
         int[][] dp = new int[N + 1][M + 1];
-
+        
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= M; j++) {
                 if (origin.charAt(i - 1) == test.charAt(j - 1)) {
@@ -25,6 +25,7 @@ public class Main {
                 }
             }
         }
+        
         return dp[N][M];
     }
 }
