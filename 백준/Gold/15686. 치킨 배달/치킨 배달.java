94c9234353c2c64e9c1eb24d3f400d10e82479ce
evan <evanbackeng@gmail.com>
2025-06-26 09:10:31 +0900

    [Gold V] Title: 치킨 배달, Time: 180 ms, Memory: 19108 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -3,32 +3,26 @@ import java.util.*;
 
 public class Main {
     static class Position {
-        final int x;
-        final int y;
-        final int type;  // 1: 가정집, 2: 치킨집
+        int x;
+        int y;
+        int type;
 
         Position(int x, int y, int type) {
             this.x = x;
             this.y = y;
             this.type = type;
         }
-
     }
-
-    static int result = Integer.MAX_VALUE;
-    static int N, M;
-    static boolean[][] visited;
     static List<Position> houseList = new ArrayList<>();
     static List<Position> chickenList = new ArrayList<>();
-
+    static int N, M;
+    static int result = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        //map 초기화
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
@@ -41,14 +35,13 @@ public class Main {
             }
         }
 
-        //백트래킹
         combination(0, new ArrayList<>());
         System.out.println(result);
     }
 
-    private static void combination(int start, List<Position> selected) {
+    static private void combination(int start, List<Position> selected) {
         if (selected.size() == M) {
-            result = Math.min(result, calculateCityChickenDistance(selected));
+            result = Math.min(result, calculateMinDist(selected));
             return;
         }
 
@@ -59,19 +52,15 @@ public class Main {
         }
     }
 
-    private static int calculateCityChickenDistance(List<Position> selectedChickens) {
-        int totalDistance = 0;
-
+    static private int calculateMinDist(List<Position> selected) {
+        int minResult = 0;
         for (Position house : houseList) {
-            int minDistance = Integer.MAX_VALUE;
-
-            for (Position chicken : selectedChickens) {
-                int distance = Math.abs(house.x - chicken.x) + Math.abs(house.y - chicken.y);
-                minDistance = Math.min(minDistance, distance);
+            int result = Integer.MAX_VALUE;
+            for (Position chicken : selected) {
+                result = Math.min(result, Math.abs(house.x - chicken.x) + Math.abs(house.y - chicken.y));
             }
-
-            totalDistance += minDistance;
+            minResult += result;
         }
-        return totalDistance;
+        return minResult;
     }
 }
