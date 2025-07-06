0a9408e480f7b83bc8b05e2d99ac2394c6dfce7c
evan <evanbackeng@gmail.com>
2025-07-07 03:14:43 +0900

    [Gold V] Title: 용액, Time: 328 ms, Memory: 28560 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int left = 0;
+        int right = N - 1;
+        int min = Integer.MAX_VALUE;
+        int ansL = arr[left];
+        int ansR = arr[right];
+
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+
+            if (Math.abs(sum) < min) {
+                min = Math.abs(sum);
+                ansL = arr[left];
+                ansR = arr[right];
+            }
+
+            if (sum > 0) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+
+        System.out.println(ansL + " " + ansR);
+    }
+}
