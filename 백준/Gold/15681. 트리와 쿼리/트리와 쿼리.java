20a3975e9ff99a2687855443e5606d525a25d8b0
evan <evanbackeng@gmail.com>
2025-07-08 09:32:53 +0900

    [Gold V] Title: 트리와 쿼리, Time: 788 ms, Memory: 72240 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
@@ -2,32 +2,20 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static class Node {
-        int next;
-
-        Node(int next) {
-            this.next = next;
-        }
-
-        @Override
-        public String toString() {
-            return "Node{next : " + next + "}";
-        }
-    }
-
-    static List<List<Node>> graph = new ArrayList<>();
-    static int[] subtreeSize;
-    static boolean[] visited;
     static int N, R, Q;
-    static StringBuilder sb = new StringBuilder();
+    static boolean[] visited;
+    static int[] subtreeSize;
+    static List<List<Integer>> graph = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        N = Integer.parseInt(st.nextToken());
-        R = Integer.parseInt(st.nextToken());
-        Q = Integer.parseInt(st.nextToken());
+        N = toInt(st.nextToken());
+        R = toInt(st.nextToken());
+        Q = toInt(st.nextToken());
+
         visited = new boolean[N + 1];
         subtreeSize = new int[N + 1];
 
@@ -37,31 +25,33 @@ public class Main {
 
         for (int i = 1; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            graph.get(start).add(new Node(end));
-            graph.get(end).add(new Node(start));
+            int start = toInt(st.nextToken()), end = toInt(st.nextToken());
+            graph.get(start).add(end);
+            graph.get(end).add(start);
         }
 
         dfs(R);
 
         for (int i = 0; i < Q; i++) {
-            st = new StringTokenizer(br.readLine());
-            int node = Integer.parseInt(st.nextToken());
-            sb.append(subtreeSize[node]).append("\n");
+            int root = toInt(br.readLine());
+            sb.append(subtreeSize[root]).append("\n");
         }
+
         bw.write(sb.toString());
         bw.close();
         br.close();
     }
 
+    private static int toInt(String str) {
+        return Integer.parseInt(str);
+    }
+
     private static int dfs(int node) {
         visited[node] = true;
         int size = 1;
-
-        for (Node child : graph.get(node)) {
-            if (!visited[child.next]) {
-                size += dfs(child.next);
+        for (int next : graph.get(node)) {
+            if (!visited[next]) {
+                size += dfs(next);
             }
         }
 
