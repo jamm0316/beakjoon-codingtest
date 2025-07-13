e45c194d17d07cd6905b1dc68396983bc79517f2
evan <evanbackeng@gmail.com>
2025-07-14 05:32:22 +0900

    [Gold V] Title: 암호 만들기, Time: 120 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,11 +1,12 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     static int L, C;
     static char[] charArr;
+    static String vowel = "aeiou";
     static StringBuilder sb = new StringBuilder();
-    static final String vowels = "aeiou";
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,14 +15,14 @@ public class Main {
 
         L = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-
         charArr = new char[C];
+
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < C; i++) {
             charArr[i] = st.nextToken().charAt(0);
         }
 
-        Arrays.sort(charArr); // 오름차순 정렬
+        Arrays.sort(charArr);
 
         backtrack(0, 0, 0, new StringBuilder());
 
@@ -30,26 +31,23 @@ public class Main {
         br.close();
     }
 
-    private static void backtrack(int start, int vowelCnt, int consonantCnt, StringBuilder result) {
+    private static void backtrack(int start, int vowelCnt, int notVowelCnt, StringBuilder result) {
         if (result.length() == L) {
-            // 최소 1개의 모음과 최소 2개의 자음 체크
-            if (vowelCnt >= 1 && consonantCnt >= 2) {
-                sb.append(result).append('\n');
+            if (vowelCnt >= 1 && notVowelCnt >= 2) {
+                sb.append(result).append("\n");
+                return;
             }
-            return;
         }
 
         for (int i = start; i < C; i++) {
             char c = charArr[i];
             result.append(c);
-            if (vowels.indexOf(c) >= 0) {
-                // 모음
-                backtrack(i + 1, vowelCnt + 1, consonantCnt, result);
+            if (vowel.indexOf(c) >= 0) {
+                backtrack(i + 1, vowelCnt + 1, notVowelCnt, result);
             } else {
-                // 자음
-                backtrack(i + 1, vowelCnt, consonantCnt + 1, result);
+                backtrack(i + 1, vowelCnt, notVowelCnt + 1, result);
             }
-            result.deleteCharAt(result.length() - 1); // 백트래킹
+            result.deleteCharAt(result.length() - 1);
         }
     }
 }
