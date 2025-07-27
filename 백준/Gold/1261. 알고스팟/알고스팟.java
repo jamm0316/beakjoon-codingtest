1ddc62436bc2447c1efd72cc507e8b410d7ff7d0
evan <evanbackeng@gmail.com>
2025-07-28 04:10:46 +0900

    [Gold IV] Title: 알고스팟, Time: 112 ms, Memory: 14564 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -3,12 +3,12 @@ import java.util.*;
 
 public class Main {
     static class Position {
-        int x, y, cost;
+        int x, y, dist;
 
-        Position(int x, int y, int cost) {
+        Position(int x, int y, int dist) {
             this.x = x;
             this.y = y;
-            this.cost = cost;
+            this.dist = dist;
         }
     }
 
@@ -16,12 +16,11 @@ public class Main {
     static int[][] map;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());  // 가로
-        M = Integer.parseInt(st.nextToken());  // 세로
+        N = Integer.parseInt(st.nextToken());  //열
+        M = Integer.parseInt(st.nextToken());  //행
 
         map = new int[M][N];
         for (int i = 0; i < M; i++) {
@@ -31,36 +30,35 @@ public class Main {
             }
         }
 
-        System.out.println(zeroOneBfs());
+        System.out.println(bfs());
     }
 
-    private static int zeroOneBfs() {
-        int[][] dist = new int[M][N];
-        for (int[] row : dist) Arrays.fill(row, Integer.MAX_VALUE);
-        Deque<Position> deque = new ArrayDeque<>();
+    private static int bfs() {
+        int[][] dp = new int[M][N];
+        for (int[] each : dp) Arrays.fill(each, Integer.MAX_VALUE);
 
-        dist[0][0] = 0;
-        deque.offerFirst(new Position(0, 0, 0));
+        Deque<Position> deque = new ArrayDeque<>();
+        deque.offerFirst(new Position(0, 0, 0));  //x, y, curDist
+        dp[0][0] = 0;
 
         while (!deque.isEmpty()) {
-            Position now = deque.pollFirst();
+            Position now = deque.poll();
 
-            for (int d = 0; d < 4; d++) {
-                int nx = now.x + dx[d], ny = now.y + dy[d];
+            for (int i = 0; i < 4; i++) {
+                int nx = now.x + dx[i], ny = now.y + dy[i];
                 if (0 <= nx && nx < M && 0 <= ny && ny < N) {
-                    int newCost = dist[now.x][now.y] + map[nx][ny];
-                    if (dist[nx][ny] > newCost) {
-                        dist[nx][ny] = newCost;
+                    int newDist = dp[now.x][now.y] + map[nx][ny];
+                    if (dp[nx][ny] > newDist) {
+                        dp[nx][ny] = newDist;
                         if (map[nx][ny] == 0) {
-                            deque.offerFirst(new Position(nx, ny, newCost));
+                            deque.offerFirst(new Position(nx, ny, now.dist));
                         } else {
-                            deque.offerLast(new Position(nx, ny, newCost));
+                            deque.offerLast(new Position(nx, ny, now.dist + 1));
                         }
                     }
                 }
             }
         }
-
-        return dist[M - 1][N - 1];
+        return dp[M - 1][N - 1];
     }
 }
