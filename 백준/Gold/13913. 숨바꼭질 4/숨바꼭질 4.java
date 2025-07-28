698d7d3bcfaec0cb2f91e9f715d631a369d21dde
evan <evanbackeng@gmail.com>
2025-07-29 06:17:14 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 248 ms, Memory: 30536 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -2,32 +2,30 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
     static int N, K;
-    static int[] dist = new int[100001];
-    static int[] parent = new int[100001];
-    static final int[] dx = {-1, 1, 2};
+    static int dist[] = new int[100_000 + 1];
+    static int parent[] = new int[100_000 + 1];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
         bfs();
 
-        // 시간 출력
         sb.append(dist[K]).append('\n');
 
-        // 경로 추적 (역순으로 넣고, 출력할 땐 정순)
         List<Integer> path = new ArrayList<>();
         int current = K;
         while (current != N) {
             path.add(current);
             current = parent[current];
         }
-        path.add(N); // 시작점 포함
+        path.add(N);
         Collections.reverse(path);
 
         for (int p : path) {
@@ -37,23 +35,20 @@ public class Main {
         System.out.println(sb);
     }
 
-    static void bfs() {
+    private static void bfs() {
         Queue<Integer> queue = new LinkedList<>();
+        queue.offer(N);
         Arrays.fill(dist, -1);
         dist[N] = 0;
-
-        queue.offer(N);
 
         while (!queue.isEmpty()) {
             int now = queue.poll();
 
-            for (int i = 0; i < 3; i++) {
-                int next = (i == 2) ? now * dx[i] : now + dx[i];
-
-                if (next >= 0 && next <= 100000 && dist[next] == -1) {
+            for (int next : new int[]{now - 1, now + 1, now * 2}) {
+                if (next >= 0 && next <= 100_000 && dist[next] == -1) {
                     dist[next] = dist[now] + 1;
                     parent[next] = now;
-                    queue.offer(next);
+                    queue.add(next);
 
                     if (next == K) return;
                 }
