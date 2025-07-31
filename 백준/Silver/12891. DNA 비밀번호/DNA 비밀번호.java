8098341c868f8a38ae725d103cb0bd028aaa2f6b
evan <evanbackeng@gmail.com>
2025-07-31 09:00:11 +0900

    [Silver II] Title: DNA 비밀번호, Time: 268 ms, Memory: 21052 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -2,62 +2,66 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] checkArr = new int[4];       // A, C, G, T 최소 조건
-    static int[] myArr = new int[4];          // 현재 윈도우 문자 개수
-    static int checkSecret = 0;               // 조건 충족한 문자 수
-
+    static int N, M, count;
+    static String DNA;
+    static int[] dx = new int[4];
+    static int[] dxTest = new int[4];;  //A’, ‘C’, ‘G’, ‘T’
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int S = Integer.parseInt(st.nextToken());
-        int P = Integer.parseInt(st.nextToken());
-        char[] A = br.readLine().toCharArray();
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        DNA = br.readLine();
+        dx = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            addChar(DNA.charAt(i));
+        }
+        if (isCount()) {
+            count++;
+        }
+
+        for (int i = M; i < N; i++) {
+            removeChar(DNA.charAt(i - M));
+            addChar(DNA.charAt(i));
+            if (isCount()) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+    private static void addChar(char c) {
+        if (c == 'A') {
+            dxTest[0]++;
+        } else if (c == 'C') {
+            dxTest[1]++;
+        } else if (c == 'G') {
+            dxTest[2]++;
+        } else if (c == 'T') {
+            dxTest[3]++;
+        }
+    }
+
+    private static void removeChar(char c) {
+        if (c == 'A') {
+            dxTest[0]--;
+        } else if (c == 'C') {
+            dxTest[1]--;
+        } else if (c == 'G') {
+            dxTest[2]--;
+        } else if (c == 'T') {
+            dxTest[3]--;
+        }
+    }
+
+    private static boolean isCount() {
         for (int i = 0; i < 4; i++) {
-            checkArr[i] = Integer.parseInt(st.nextToken());
-            if (checkArr[i] == 0) checkSecret++;  // 0이면 자동 충족
+            if (dx[i] > dxTest[i]) {
+                return false;
+            }
         }
-
-        // 초기 윈도우 설정
-        for (int i = 0; i < P; i++) {
-            add(A[i]);
-        }
-
-        int result = 0;
-        if (checkSecret == 4) result++;
-
-        // 슬라이딩 윈도우
-        for (int i = P; i < S; i++) {
-            int j = i - P;
-            add(A[i]);
-            remove(A[j]);
-            if (checkSecret == 4) result++;
-        }
-
-        System.out.println(result);
-    }
-
-    // 문자 추가
-    private static void add(char c) {
-        int idx = toIndex(c);
-        myArr[idx]++;
-        if (myArr[idx] == checkArr[idx]) checkSecret++;
-    }
-
-    // 문자 제거
-    private static void remove(char c) {
-        int idx = toIndex(c);
-        if (myArr[idx] == checkArr[idx]) checkSecret--;
-        myArr[idx]--;
-    }
-
-    // 문자 → 인덱스 변환
-    private static int toIndex(char c) {
-        if (c == 'A') return 0;
-        else if (c == 'C') return 1;
-        else if (c == 'G') return 2;
-        else return 3;
+        return true;
     }
 }
