77674136e8a6eed1b6ce9c110afc23b4e812ca7c
evan <evanbackeng@gmail.com>
2025-09-01 06:11:08 +0900

    Update P17404.java

--- DP/골드/골드4_17404_RGB 거리/P17404.java
+++ DP/골드/골드4_17404_RGB 거리/P17404.java
@@ -26,12 +26,14 @@ public class Main {
                 dp[0][c] = (c == start) ? cost[0][c] : INF;
             }
 
+            //2. 1 ~ N번 째 누적 cost 갱신
             for (int i = 1; i < N; i++) {
                 dp[i][0] = cost[i][0] + Math.min(dp[i - 1][1], dp[i - 1][2]);
                 dp[i][1] = cost[i][1] + Math.min(dp[i - 1][0], dp[i - 1][2]);
                 dp[i][2] = cost[i][2] + Math.min(dp[i - 1][0], dp[i - 1][1]);
             }
 
+            //3. N번째가 시작 색깔과 같지 않을 경우 answer 갱신
             for (int end = 0; end < 3; end++) {
                 if (end == start) continue;
                 answer = Math.min(answer, dp[N - 1][end]);
