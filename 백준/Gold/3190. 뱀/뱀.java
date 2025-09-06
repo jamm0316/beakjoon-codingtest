9f362d3d8d03d3cdc48b8fda51da54cdb16e0301
evan <evanbackeng@gmail.com>
2025-09-07 05:50:33 +0900

    [Gold IV] Title: 뱀, Time: 116 ms, Memory: 14704 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.java
+++ 백준/Gold/3190. 뱀/뱀.java
@@ -3,9 +3,9 @@ import java.util.*;
 
 public class Main {
     static int N, K, L, runningTime;
-    static int[][] map; // 0: 빈 칸, 1: 사과, 2: 뱀
+    static int[][] map;  //0: 이동 가능, 1: 사과, 2: 뱀 몽길이
+    static boolean running = true;
     static Snake snake;
-    static Queue<Command> commands;
 
     enum Dir {N, S, W, E}
 
@@ -20,48 +20,48 @@ public class Main {
     }
 
     static class Snake {
-        Deque<int[]> body; // 뱀의 몸통 위치 (머리: 마지막, 꼬리: 첫 번째)
+        Deque<int[]> body;  //머리: 마지막, 꼬리: 첫번쨰
         Dir direction;
 
         Snake(int[] initPosition) {
             body = new LinkedList<>();
             body.offerLast(initPosition);
-            direction = Dir.E; // 초기 방향: 오른쪽
+            direction = Dir.E;
         }
 
-        int[] head() {
+        public int[] head() {
             return body.peekLast();
         }
 
-        int[] tail() {
+        public int[] tail() {
             return body.peekFirst();
         }
 
-        // 사과를 먹었을 때: 머리만 추가
-        void grow(int x, int y) {
-            body.offerLast(new int[]{x, y});
+        //사과 먹었을 때: 머리만 추가
+        public void grow(int x, int y) {
+            body.offer(new int[]{x, y});
             map[x][y] = 2;
         }
 
-        // 사과가 없을 때: 머리 추가, 꼬리 제거
-        void move(int x, int y) {
-            body.offerLast(new int[]{x, y});
-            map[x][y] = 2;
-            int[] tail = body.pollFirst();
-            map[tail[0]][tail[1]] = 0;
+        //사과 없을 때: 머리 추가, 꼬리 제거
+        public void move(int x, int y) {
+            body.offer(new int[]{x, y});
+            map[x][y] = 2;  //몸 표시
+            int[] tail = body.pollFirst();  //꼬리제거
+            map[tail[0]][tail[1]] = 0; //맵에서 지우기
         }
 
-        // 방향 전환
-        void changeDirection(char command) {
-            if (command == 'D') { // 오른쪽 90도
+        //방향전환
+        public void changeDirection(char command) {
+            if (command == 'D') {  //오른쪽 90도
                 if (direction == Dir.N) direction = Dir.E;
-                else if (direction == Dir.E) direction = Dir.S;
                 else if (direction == Dir.S) direction = Dir.W;
+                else if (direction == Dir.E) direction = Dir.S;
                 else if (direction == Dir.W) direction = Dir.N;
-            } else if (command == 'L') { // 왼쪽 90도
+            } else if (command == 'L') {
                 if (direction == Dir.N) direction = Dir.W;
-                else if (direction == Dir.E) direction = Dir.N;
                 else if (direction == Dir.S) direction = Dir.E;
+                else if (direction == Dir.E) direction = Dir.N;
                 else if (direction == Dir.W) direction = Dir.S;
             }
         }
@@ -72,20 +72,18 @@ public class Main {
         N = Integer.parseInt(br.readLine());
         map = new int[N][N];
         snake = new Snake(new int[]{0, 0});
-        map[0][0] = 2; // 뱀 초기 위치
+        map[0][0] = 2;
 
-        // 사과 입력
         K = Integer.parseInt(br.readLine());
         for (int i = 0; i < K; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int row = Integer.parseInt(st.nextToken()) - 1; // 1-based to 0-based
+            int row = Integer.parseInt(st.nextToken()) - 1;
             int col = Integer.parseInt(st.nextToken()) - 1;
-            map[row][col] = 1; // 사과
+            map[row][col] = 1;  //사과
         }
 
-        // 방향 전환 명령 입력
         L = Integer.parseInt(br.readLine());
-        commands = new LinkedList<>();
+        Queue<Command> commands = new LinkedList<>();
         for (int i = 0; i < L; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int time = Integer.parseInt(st.nextToken());
@@ -93,11 +91,11 @@ public class Main {
             commands.offer(new Command(time, dir));
         }
 
-        // 게임 시작
+        // 게임
         runningTime = 0;
         while (true) {
             runningTime++;
-            
+
             // 다음 위치 계산
             int[] nextMove = getNextMove(snake.direction);
             int nx = snake.head()[0] + nextMove[0];
