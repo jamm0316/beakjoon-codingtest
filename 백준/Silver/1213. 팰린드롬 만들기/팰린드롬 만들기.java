dd9b3d4f3228585c3d6de653ac9dfa3344d2b0c5
evan <evanbackeng@gmail.com>
2025-10-24 05:43:55 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 100 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
@@ -1,31 +1,39 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char[] s = br.readLine().toCharArray();
+        char[] arr = br.readLine().toCharArray();
 
-        int[] cnt = new int[26];
-        for (char c : s) cnt[c - 'A']++;
+        int[] alphabet = new int[26];
+        for (char c : arr) alphabet[c - 'A']++;
 
         int odd = 0, oddIdx = -1;
         for (int i = 0; i < 26; i++) {
-            if ((cnt[i] & 1) == 1) { odd++; oddIdx = i; }
+            if (alphabet[i] % 2 != 0) {
+                odd++;
+                oddIdx = i;
+            }
         }
+
         if (odd > 1) {
             System.out.println("I'm Sorry Hansoo");
             return;
         }
 
-        StringBuilder left = new StringBuilder(s.length / 2);
+        StringBuilder sb = new StringBuilder(arr.length / 2);
         for (int i = 0; i < 26; i++) {
-            int pairs = cnt[i] / 2;
-            for (int k = 0; k < pairs; k++) left.append((char)('A' + i));
+            int pairs = alphabet[i] / 2;
+            for (int j = 0; j < pairs; j++) {
+                sb.append((char) ('A' + i));
+            }
         }
 
-        String mid = (odd == 1) ? String.valueOf((char)('A' + oddIdx)) : "";
+        String mid = (odd == 1) ? String.valueOf((char) ('A' + oddIdx)) : "";
 
-        StringBuilder right = new StringBuilder(left).reverse();
-        System.out.print(left.toString() + mid + right.toString());
+        StringBuilder right = new StringBuilder(sb).reverse();
+        sb.append(mid).append(right);
+        System.out.println(sb);
     }
 }
