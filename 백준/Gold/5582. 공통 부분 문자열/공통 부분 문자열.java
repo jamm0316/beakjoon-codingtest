79d1a6467ec52c96833180ec3ac39a43b67f39f4
evan <evanbackeng@gmail.com>
2025-10-26 04:54:56 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 224 ms, Memory: 77024 KB -BaekjoonHub

--- 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.java
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.java
@@ -5,23 +5,21 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str1 = br.readLine();
         String str2 = br.readLine();
-        
-        int len1 = str1.length();
-        int len2 = str2.length();
-        int[][] dp = new int[len1 + 1][len2 + 1];
+
+        int N = str1.length();
+        int M = str2.length();
+
+        int[][] dp = new int[N + 1][M + 1];
         int maxLen = 0;
-        
-        // dp[i][j] = str1의 i번째 문자와 str2의 j번째 문자로 끝나는 
-        //            공통 부분 문자열의 길이
-        for (int i = 1; i <= len1; i++) {
-            for (int j = 1; j <= len2; j++) {
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
                 if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                     maxLen = Math.max(maxLen, dp[i][j]);
                 }
             }
         }
-        
         System.out.println(maxLen);
     }
 }
