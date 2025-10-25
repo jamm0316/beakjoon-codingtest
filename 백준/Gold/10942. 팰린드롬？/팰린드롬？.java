d14da480abdd8904937291d27d67264f48257d05
evan <evanbackeng@gmail.com>
2025-10-25 11:37:02 +0900

    [Gold IV] Title: 팰린드롬?, Time: 748 ms, Memory: 217960 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -2,35 +2,41 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
-    static int[] arr;
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N];
+        StringBuilder out = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] a = new int[N + 1];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= N; i++) a[i] = Integer.parseInt(st.nextToken());
+
+        // DP: dp[s][e] = [s..e]가 팰린드롬인가?
+        boolean[][] dp = new boolean[N + 1][N + 1];
+
+        // 길이 1
+        for (int i = 1; i <= N; i++) dp[i][i] = true;
+
+        // 길이 2
+        for (int i = 1; i < N; i++) {
+            if (a[i] == a[i + 1]) dp[i][i + 1] = true;
         }
 
-        M = Integer.parseInt(br.readLine());
-        int[][] query = new int[M][2];
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < M; i++) {
-            query[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            int start = query[i][0] - 1, end = query[i][1] - 1;
-            boolean isTrue = true;
-            for (int j = 0; j < end - start; j++) {
-                if (arr[start + j] != arr[end - j]) {
-                    isTrue = false;
-                    sb.append(0).append('\n');
-                    break;
-                }
-            }
-            if (isTrue) {
-                sb.append(1).append('\n');
+        // 길이 3 이상
+        for (int len = 3; len <= N; len++) {
+            for (int s = 1; s + len - 1 <= N; s++) {
+                int e = s + len - 1;
+                if (a[s] == a[e] && dp[s + 1][e - 1]) dp[s][e] = true;
             }
         }
-        System.out.println(sb);
+
+        int M = Integer.parseInt(br.readLine());
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            out.append(dp[s][e] ? 1 : 0).append('\n');
+        }
+        System.out.print(out);
     }
 }
