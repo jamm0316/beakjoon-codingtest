350225cf9bf348e95390508ff98dd86914ed7ee2
evan <evanbackeng@gmail.com>
2025-12-26 05:56:46 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 512 ms, Memory: 66572 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
@@ -1,39 +1,39 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+class Main {
     private static int N;
     private static int K;
     private static int[] arr;
     public static void main(String[] args) throws IOException {
-        inputWork();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
         
+
+        Map<Integer, Integer> map = new HashMap<>();
         int left = 0;
         int right = 1;
-        Map<Integer, Integer> numberMap = new HashMap<>();
-        numberMap.put(arr[left], 1);
         int maxLength = 0;
-        while(right < N) {
-            numberMap.put(arr[right], numberMap.getOrDefault(arr[right], 0) + 1);
 
-            while (numberMap.get(arr[right]) > K) {
-                numberMap.put(arr[left], numberMap.get(arr[left]) - 1);
-                if (numberMap.get(arr[left]) == 0) {
-                    numberMap.remove(arr[left]);
-                }
+        map.put(arr[left], 1);
+        while (right < N) {
+            map.put(arr[right], map.getOrDefault(arr[right], 0) + 1);
+            while (map.get(arr[right]) > K) {
+                map.put(arr[left], map.get(arr[left]) - 1);
                 left++;
+                if (map.get(arr[left]) == 0) {
+                    map.remove(arr[left]);
+                }
             }
             maxLength = Math.max(maxLength, right - left + 1);
             right++;
         }
         System.out.println(maxLength);
-    }
-    
-    private static void inputWork() throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
     }
 }
