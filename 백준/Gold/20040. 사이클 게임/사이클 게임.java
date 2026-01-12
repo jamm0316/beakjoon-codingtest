9c9ec266d0b38d8aa27d0892941408ab804a5e22
송재명(evan) <evanbackeng@gmail.com>
2026-01-12 09:45:00 +0900

    [Gold IV] Title: 사이클 게임, Time: 640 ms, Memory: 131968 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] parent;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+        
+        int answer = 0;
+        
+        for (int i = 1; i <= m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            
+            if (find(a) == find(b)) {
+                answer = i;
+                break;
+            }
+            
+            union(a, b);
+        }
+        
+        System.out.println(answer);
+    }
+    
+    static int find(int x) {
+        if (parent[x] == x) {
+            return x;
+        }
+        return parent[x] = find(parent[x]);
+    }
+    
+    static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        
+        if (a != b) {
+            parent[b] = a;
+        }
+    }
+}
